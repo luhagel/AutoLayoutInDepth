1931908e412307f12e359e0a0a1055f8cec7384b
danstepanov <danhstepanov@gmail.com>
2016-07-21 09:43:58 -0700

    initial commit

---
+++ AutoLayoutInDepth/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  AutoLayoutInDepth
+//
+//  Created by Daniel Stepanov on 7/21/16.
+//  Copyright © 2016 Daniel Stepanov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
